a35cc384b3e1c67d387e5a6065f0fb3845b14de1
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-11-23 17:38:52 +0530

    adding icons

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,13 +1,37 @@
+import { FaUserAlt } from "react-icons/fa";
+import { ImLocation } from "react-icons/im";
+import { MdWork } from "react-icons/md";
+import { BsTelephoneFill } from "react-icons/bs";
+import { GrMail } from "react-icons/gr";
+import { MdWeb } from "react-icons/md";
 const Profile = ({ name, company, address, phone, email, website, image }) => {
   return (
     <div className="profile">
-      <img loading="lazy" src={image} />
-      <div className="name">Name : {name}</div>
-      <div className="address"> Adress:{address}</div>
-      <div className="company">Company:{company}</div>
-      <div className="phone">Phone:{phone}</div>
-      <div className="email">email:{email}</div>
-      <div className="website">website:{website}</div>
+      <img loading="lazy" src={image} alt="image loading error" />
+      <div className="name">
+        <FaUserAlt /> {name}
+      </div>
+      <div className="address">
+        {" "}
+        <ImLocation />
+        {address}
+      </div>
+      <div className="company">
+        <MdWork /> {company}
+      </div>
+      <div className="phone">
+        {" "}
+        <BsTelephoneFill />
+        {phone}
+      </div>
+      <div className="email">
+        <GrMail />
+        {email}
+      </div>
+      <div className="website">
+        <MdWeb />
+        {website}
+      </div>
     </div>
   );
 };
